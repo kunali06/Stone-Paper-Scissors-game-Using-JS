b7702cb6c1f90dc8a9338c469668ef830e7a23f0
KUNALI BHAVARTHE <kunalibhavarthe2003@gmail.com>
2026-01-05 23:03:16 +0530

    Implement player selection and result checking

--- stonePaper.js
+++ stonePaper.js
@@ -1,1 +1,33 @@
+ let p1 = "";
+  let p2 = "";
 
+  function selectP1(choice) {
+    p1 = choice;
+    checkResult();
+  }
+
+  function selectP2(choice) {
+    p2 = choice;
+    checkResult();
+  }
+
+  function checkResult() {
+    if (!p1 || !p2) return;
+
+    if (p1 === p2) {
+      result.innerText = "Draw 😐";
+    } 
+    else if (
+      (p1 === "stone" && p2 === "scissor") ||
+      (p1 === "paper" && p2 === "stone") ||
+      (p1 === "scissor" && p2 === "paper")
+    ) {
+      result.innerText = "Player 1 Wins 🎉";
+    } 
+    else {
+      result.innerText = "Player 2 Wins 🎉";
+    }
+
+    p1 = "";
+    p2 = "";
+  }
